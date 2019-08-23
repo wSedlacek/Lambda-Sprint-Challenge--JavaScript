6edde2a56637a9b00571a82cb77b724c270d1e15
William Sedlacek <wsedlacekc@gmail.com>
2019-08-23 10:52:11 -0500

    Complete Functions Challenge

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,23 +6,29 @@
  * The last parameter accepts a callback
  * The consume function should return the invocation of cb, passing a and b into cb as arguments
  */
+const consume = (a, b, cb) => cb(a, b);
 
 /* Step 2: Create several functions to callback with consume();
  * Create a function named add that returns the sum of two numbers
  * Create a function named multiply that returns the product of two numbers
  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
  */
+const add = (num1, num2) => num1 + num2;
+const multiply = (num1, num2) => num1 * num2;
+const greeting = (firstName, lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`;
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume('Mary', 'Poppins', greeting)); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ====
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
 // Explanation:
+// Scope words from the inside out. The most nested function has acess to all it's data and all the data of every scope it is nested in, however other functions can't reach in to touch the nested functions data.
+// In this example internal is defined in `myFunction()` as such everything in myFunction, including nestedFunciton has access to it.
 
 const external = "I'm outside the function";
 
